f14ed163837ab0647d191fc0791947ac3bca6d7a
KOBATATU <kobatatu258@yahoo.co.jp>
2024-12-07 15:17:43 +0900

    remove: logging query

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
